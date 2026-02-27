5a1215ace9553ab02ec50b62b1b3cd67d490a3c6
joaquin.s <custodiamdevelopment.js@gmail.com>
2026-02-27 20:03:39 -0300

    database config

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,7 @@
-import pkg from 'pg';
+/* import pkg from 'pg';
 import { config } from '../config/env.js';
 
 const { Pool } = pkg;
-
-/* const pool = new Pool({
-  connectionString: config.databaseUrl
-}); */
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -13,6 +9,18 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});
+
+export default pool;
+ */
+import pkg from 'pg';
+import { config } from '../config/env.js';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: config.databaseUrl,
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
